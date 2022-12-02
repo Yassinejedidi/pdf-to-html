a36541cbac5d24fbe455accc1dcb8e6bbbe77b55
Yassinejedidi <Yassinejedidi10@gmail.com>
2022-12-02 04:02:15 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,195 @@
+var https = require("https");
+var path = require("path");
+var fs = require("fs");
+
+// `request` module is required for file upload.
+// Use "npm install request" command to install.
+var request = require("request");
+
+// The authentication key (API Key).
+// Get your own by registering at https://app.pdf.co
+const API_KEY = "skanlau13@gmail.com_948a7ddeeaf9e03d1761eb331598dca4958be77c87c7a4118c9822e5c35cdc7e55f1ccd1";
+
+// Source PDF file
+const SourceFile = "./novembre.pdf";
+// Comma-separated list of page indices (or ranges) to process. Leave empty for all pages. Example: '0,2-5,7-'.
+const Pages = "";
+// PDF document password. Leave empty for unprotected documents.
+const Password = "";
+// Destination HTML file name
+const DestinationFile = "./result.html";
+// Set to `true` to get simplified HTML without CSS. Default is the rich HTML keeping the document design.
+const PlainHtml = false;
+// Set to `true` if your document has the column layout like a newspaper.
+const ColumnLayout = false;
+
+
+// 1. RETRIEVE PRESIGNED URL TO UPLOAD FILE.
+getPresignedUrl(API_KEY, SourceFile)
+    .then(([uploadUrl, uploadedFileUrl]) => {
+        // 2. UPLOAD THE FILE TO CLOUD.
+        uploadFile(API_KEY, SourceFile, uploadUrl)
+            .then(() => {
+                // 3. CONVERT UPLOADED PDF FILE TO HTML
+                convertPdfToHtml(API_KEY, uploadedFileUrl, Password, Pages, PlainHtml, ColumnLayout, DestinationFile);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    })
+    .catch(e => {
+        console.log(e);
+    });
+
+
+function getPresignedUrl(apiKey, localFile) {
+    return new Promise(resolve => {
+        // Prepare request to `Get Presigned URL` API endpoint
+        let queryPath = `/v1/file/upload/get-presigned-url?contenttype=application/octet-stream&name=${path.basename(SourceFile)}`;
+        let reqOptions = {
+            host: "api.pdf.co",
+            path: encodeURI(queryPath),
+            headers: { "x-api-key": API_KEY }
+        };
+        // Send request
+        https.get(reqOptions, (response) => {
+            response.on("data", (d) => {
+                let data = JSON.parse(d);
+                if (data.error == false) {
+                    // Return presigned url we received
+                    resolve([data.presignedUrl, data.url]);
+                }
+                else {
+                    // Service reported error
+                    console.log("getPresignedUrl(): " + data.message);
+                }
+            });
+        })
+            .on("error", (e) => {
+                // Request error
+                console.log("getPresignedUrl(): " + e);
+            });
+    });
+}
+
+function uploadFile(apiKey, localFile, uploadUrl) {
+    return new Promise(resolve => {
+        fs.readFile(SourceFile, (err, data) => {
+            request({
+                method: "PUT",
+                url: uploadUrl,
+                body: data,
+                headers: {
+                    "Content-Type": "application/octet-stream"
+                }
+            }, (err, res, body) => {
+                if (!err) {
+                    resolve();
+                }
+                else {
+                    console.log("uploadFile() request error: " + e);
+                }
+            });
+        });
+    });
+}
+
+function convertPdfToHtml(apiKey, uploadedFileUrl, password, pages, plainHtml, columnLayout, destinationFile) {
+    // Prepare request to `PDF To HTML` API endpoint
+    var queryPath = `/v1/pdf/convert/to/html`;
+
+    // JSON payload for api request
+    var jsonPayload = JSON.stringify({
+        name: path.basename(destinationFile), password: password, pages: pages, simple: plainHtml, columns: columnLayout, url: uploadedFileUrl, async: true
+    });
+
+    var reqOptions = {
+        host: "api.pdf.co",
+        method: "POST",
+        path: queryPath,
+        headers: {
+            "x-api-key": apiKey,
+            "Content-Type": "application/json",
+            "Content-Length": Buffer.byteLength(jsonPayload, 'utf8')
+        }
+    };
+    // Send request
+    var postRequest = https.request(reqOptions, (response) => {
+        response.on("data", (d) => {
+            response.setEncoding("utf8");
+
+            // Parse JSON response
+            let data = JSON.parse(d);
+            console.log(`Job #${data.jobId} has been created!`);
+
+            if (data.error == false) {
+                checkIfJobIsCompleted(data.jobId, data.url, destinationFile);
+            }
+            else {
+                // Service reported error
+                console.log("convertPdfToHtml(): " + data.message);
+            }
+        });
+    })
+        .on("error", (e) => {
+            // Request error
+            console.log("convertPdfToHtml(): " + e);
+        });
+
+    // Write request data
+    postRequest.write(jsonPayload);
+    postRequest.end();
+}
+
+function checkIfJobIsCompleted(jobId, resultFileUrl, destinationFile) {
+    let queryPath = `/v1/job/check`;
+
+    // JSON payload for api request
+    let jsonPayload = JSON.stringify({
+        jobid: jobId
+    });
+
+    let reqOptions = {
+        host: "api.pdf.co",
+        path: queryPath,
+        method: "POST",
+        headers: {
+            "x-api-key": API_KEY,
+            "Content-Type": "application/json",
+            "Content-Length": Buffer.byteLength(jsonPayload, 'utf8')
+        }
+    };
+
+    // Send request
+    var postRequest = https.request(reqOptions, (response) => {
+        response.on("data", (d) => {
+            response.setEncoding("utf8");
+
+            // Parse JSON response
+            let data = JSON.parse(d);
+            console.log(`Checking Job #${jobId}, Status: ${data.status}, Time: ${new Date().toLocaleString()}`);
+
+            if (data.status == "working") {
+                // Check again after 3 seconds
+                setTimeout(function () { checkIfJobIsCompleted(jobId, resultFileUrl, destinationFile); }, 3000);
+            }
+            else if (data.status == "success") {
+                // Download HTML file
+                var file = fs.createWriteStream(destinationFile);
+                https.get(resultFileUrl, (response2) => {
+                    response2.pipe(file)
+                        .on("close", () => {
+                            console.log(`Generated HTML file saved as "${destinationFile}" file.`);
+                        });
+                });
+            }
+            else {
+                console.log(`Operation ended with status: "${data.status}".`);
+            }
+        })
+    });
+
+    // Write request data
+    postRequest.write(jsonPayload);
+    postRequest.end();
+}
